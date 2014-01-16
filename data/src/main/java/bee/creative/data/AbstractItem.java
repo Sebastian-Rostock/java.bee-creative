e378d4c72ed73cd082a6856cfa6f669a866ea0b5
Sebastian Rostock <bee-creative@gmx.de>
2014-01-16 23:31:11 +0100

    refactor Items, AbstractItem/-Pool

---
+++ data/src/main/java/bee/creative/data/AbstractItem.java
@@ -0,0 +1,165 @@
+package bee.creative.data;
+
+import bee.creative.util.Assigner;
+import bee.creative.util.Assignment;
+import bee.creative.util.Converter;
+import bee.creative.util.Field;
+import bee.creative.util.Filters;
+import bee.creative.util.Iterables;
+import bee.creative.util.Objects;
+
+/**
+ * Diese Klasse implementiert ein abstraktes {@link Item}, dass seinen {@link AbstractPool} kennt und einen Teil seiner Schnittstelle an diesen Delegiert. <br>
+ * Die Methoden {@link #append()}, {@link #remove()} und {@link #update()} delegieren an {@link AbstractPool#append(AbstractItem)},
+ * {@link AbstractPool#remove(AbstractItem)} bzw. {@link AbstractPool#update(AbstractItem)}. <br>
+ * Der {@link #hashCode() Streuwert} basiert auf dem {@link #key() Schlüssel}, die {@link #equals(Object) Äquivalenz} basiert auf der von {@link #key()
+ * Schlüssel} und {@link #pool() Pool}.
+ * 
+ * @author [cc-by] 2013 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/]
+ */
+public abstract class AbstractItem implements Item {
+
+	/**
+	 * Dieses Feld speichert den {@link Converter} für {@link #assigners(Assignment)}, welcher seine Eingabe ({@link Field}) als {@link Assigner} zurück gibt,
+	 * sofern diese ein solcher ist ({@code instanceof}). Andernfalls wird {@code null} zurück gegeben.
+	 */
+	protected static final Converter<Field<?, ?>, Assigner<? super Item, ? super Item>> FIELD_ASSIGNER_CONVERTER =
+		new Converter<Field<?, ?>, Assigner<? super Item, ? super Item>>() {
+
+			@SuppressWarnings ("unchecked")
+			@Override
+			public Assigner<? super Item, ? super Item> convert(final Field<?, ?> input) {
+				return input instanceof Assigner<?, ?> ? (Assigner<? super Item, ? super Item>)input : null;
+			}
+
+		};
+
+	/**
+	 * Diese Methode gibt die {@link Assigner} zurück, die in {@link #assign(Assignment)} zur Übertragung der Informatioenen des gegebenen {@link Item}s auf
+	 * dieses {@link Item} verwendet werden.
+	 * <p>
+	 * Die Implementation in {@link AbstractItem} verwndet hierfür die am {@link #type()} dieses bzw. des gegebenen {@link Item}s definierten {@link Field}s, die
+	 * die {@link Assigner}-Schnittstelle implementieren. Die genutzten {@link Field}s ergeben sich aus:
+	 * {@code this.type().is(value.type()) ? value.type().fields() : value.type().is(this.type()) ? this.type().fields() : Iterables.voidIterable())}.
+	 * 
+	 * @see #FIELD_ASSIGNER_CONVERTER
+	 * @param assignment {@link Item} als Quellobjekt des in {@link #assign(Assignment)} gegebenen {@link Assignment}s.
+	 * @return {@link Assigner}s.
+	 */
+	protected Iterable<? extends Assigner<? super Item, ? super Item>> assigners(final Assignment<? extends Item> assignment) {
+		final Type<?> thisType = this.type(), thatType = assignment.value().type();
+		return Iterables.filteredIterable(Filters.nullFilter(), Iterables.convertedIterable(AbstractItem.FIELD_ASSIGNER_CONVERTER, //
+			thisType.is(thatType) ? thatType.fields() : thatType.is(thisType) ? thisType.fields() : Iterables.<Field<?, ?>>voidIterable()));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public abstract AbstractPool<? extends Item> pool();
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Type<?> type() {
+		return this.pool().type();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Object owner() {
+		return this.pool().owner();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AbstractPool#state(AbstractItem)
+	 */
+	@Override
+	public int state() {
+		return this.pool().state(this);
+	}
+
+	/**
+	 * {@inheritDoc} Hierbei werden die {@link Assigner} verwendet, die über die Methode {@link #assigners(Assignment)} aus dem {@link Item} des gegebenen
+	 * {@link Assignment}s ({@link Assignment#value() Quellobjekt}) ermittelt werden.
+	 * 
+	 * @see #assigners(Assignment)
+	 * @see Assignment#assign(Object, Object, Assigner)
+	 */
+	@Override
+	public void assign(final Assignment<? extends Item> assignment) throws NullPointerException, IllegalArgumentException {
+		if(assignment == null) throw new NullPointerException();
+		final Item value = assignment.value();
+		if(value == null) throw new IllegalArgumentException();
+		for(final Assigner<? super Item, ? super Item> assigner: this.assigners(assignment)){
+			assigner.assign(this, assignment);
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AbstractPool#append(AbstractItem)
+	 * @see AbstractPool#doAppend(Item)
+	 */
+	@Override
+	public void append() {
+		this.pool().append(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AbstractPool#remove(AbstractItem)
+	 * @see AbstractPool#doRemove(Item)
+	 */
+	@Override
+	public void remove() throws IllegalStateException {
+		this.pool().remove(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see AbstractPool#update(AbstractItem)
+	 * @see AbstractPool#doUpdate(Item)
+	 */
+	@Override
+	public void update() throws IllegalStateException {
+		this.pool().update(this);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		final long value = this.key();
+		return (int)(value ^ (value >>> 32));
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(final Object object) {
+		if(object == this) return true;
+		if(!(object instanceof Item)) return false;
+		final Item data = (Item)object;
+		return (this.key() == data.key()) && Objects.equals(this.pool(), data.pool());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return Objects.toStringCall(this);
+	}
+
+}
---
+++ data/src/main/java/bee/creative/data/AbstractPool.java
@@ -0,0 +1,298 @@
+package bee.creative.data;
+
+import java.util.Collection;
+import java.util.Iterator;
+import bee.creative.util.Converters;
+import bee.creative.util.Field;
+import bee.creative.util.Filters;
+import bee.creative.util.Iterables;
+import bee.creative.util.Iterables.FilteredIterable;
+import bee.creative.util.Iterators;
+import bee.creative.util.Objects;
+
+/**
+ * Diese Klasse implementiert einen {@link Pool}. Die Datensätze müssen Nachfahren von {@link AbstractItem} sein.
+ * 
+ * @author Sebastian Rostock 2011.
+ * @param <GItem> Typ der Datensätze.
+ */
+public abstract class AbstractPool<GItem extends Item> implements Pool<GItem> {
+
+	/**
+	 * Diese Klasse implementiert eine {@link Selection}, die ein {@link FilteredIterable} verwendet.
+	 * 
+	 * @author [cc-by] 2013 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/]
+	 * @param <GItem> Typ der Datensätze.
+	 */
+	public static final class FilteredSelection<GItem> implements Selection<GItem> {
+
+		/**
+		 * Dieses Feld speichert das {@link Iterable}.
+		 */
+		final Iterable<? extends GItem> iterable;
+
+		/**
+		 * Dieser Konstruktor initialisiert das {@link Iterable}.
+		 * 
+		 * @param iterable {@link Iterable} der {@link Item}s.
+		 * @throws NullPointerException Wenn die Eingabe {@code null} ist.
+		 */
+		public FilteredSelection(final Iterable<? extends GItem> iterable) throws NullPointerException {
+			if(iterable == null) throw new NullPointerException();
+			this.iterable = iterable;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * <p>
+		 * Diese Implementation gibt das erste {@link Item} der {@link Selection} zurück, die von {@link #findAll(Field, Object)} für die gegebenen Parameter
+		 * geliefert wird.
+		 */
+		@Override
+		public <GValue> GItem find(final Field<? super GItem, ? extends GValue> field, final GValue value) throws NullPointerException {
+			for(final GItem item: this.findAll(field, value))
+				return item;
+			return null;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public <GValue> Selection<GItem> findAll(final Field<? super GItem, ? extends GValue> field, final GValue value) throws NullPointerException {
+			if(field == null) throw new NullPointerException();
+			return new FilteredSelection<GItem>(Iterables.filteredIterable(Filters.convertedFilter(Converters.fieldConverter(field), Filters.containsFilter(value)),
+				this.iterable));
+		}
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@SuppressWarnings ("unchecked")
+		@Override
+		public Iterator<GItem> iterator() {
+			return (Iterator<GItem>)this.iterable.iterator();
+		}
+
+	}
+
+	/**
+	 * Diese Methode implementiert {@link AbstractItem#state()}.
+	 * 
+	 * @param item {@link AbstractItem}.
+	 * @return Status.
+	 * @throws NullPointerException Wenn die Eingabe {@code null} ist.
+	 */
+	protected abstract int state(final AbstractItem item) throws NullPointerException;
+
+	/**
+	 * Diese Methode implementiert {@link AbstractItem#append()}.
+	 * 
+	 * @param item {@link AbstractItem}.
+	 * @throws NullPointerException Wenn die Eingabe {@code null} ist.
+	 * @throws IllegalArgumentException Wenn das {@link AbstractItem} nicht zu diesem {@link AbstractPool} gehört oder {@link Item#state()} unbekannt ist.
+	 */
+	@SuppressWarnings ("unchecked")
+	protected final void append(final AbstractItem item) throws NullPointerException, IllegalArgumentException {
+		switch(item.state()){
+			case Item.CREATE_STATE:
+			case Item.REMOVE_STATE:
+			case Item.UPDATE_STATE:
+				if(!this.equals(item.pool())) throw new IllegalArgumentException();
+				this.doAppend((GItem)item);
+			case Item.APPEND_STATE:
+				return;
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * Diese Methode implementiert {@link AbstractItem#remove()}.
+	 * 
+	 * @param item {@link AbstractItem}.
+	 * @throws NullPointerException Wenn die Eingabe {@code null} ist.
+	 * @throws IllegalStateException Wenn sich das {@link Item} im Status {@link Item#CREATE_STATE} befindet.
+	 * @throws IllegalArgumentException Wenn das {@link AbstractItem} nicht zu diesem {@link AbstractPool} gehört oder {@link Item#state()} unbekannt ist.
+	 */
+	@SuppressWarnings ("unchecked")
+	protected final void remove(final AbstractItem item) throws NullPointerException, IllegalStateException, IllegalArgumentException {
+		switch(item.state()){
+			case Item.APPEND_STATE:
+			case Item.UPDATE_STATE:
+				if(!this.equals(item.pool())) throw new IllegalArgumentException();
+				this.doRemove((GItem)item);
+			case Item.REMOVE_STATE:
+				return;
+			case Item.CREATE_STATE:
+				throw new IllegalStateException();
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * Diese Methode implementiert {@link AbstractItem#remove()}.
+	 * 
+	 * @param item {@link AbstractItem}.
+	 * @throws NullPointerException Wenn die Eingabe {@code null} ist.
+	 * @throws IllegalStateException Wenn sich das {@link Item} im Status {@link Item#APPEND_STATE} befindet.
+	 * @throws IllegalArgumentException Wenn das {@link AbstractItem} nicht zu diesem {@link AbstractPool} gehört oder {@link Item#state()} unbekannt ist.
+	 */
+	@SuppressWarnings ("unchecked")
+	protected final void update(final AbstractItem item) throws NullPointerException, IllegalStateException, IllegalArgumentException {
+		switch(item.state()){
+			case Item.REMOVE_STATE:
+			case Item.CREATE_STATE:
+				if(!this.equals(item.pool())) throw new IllegalArgumentException();
+				this.doUpdate((GItem)item);
+			case Item.UPDATE_STATE:
+				return;
+			case Item.APPEND_STATE:
+				throw new IllegalStateException();
+			default:
+				throw new IllegalArgumentException();
+		}
+	}
+
+	/**
+	 * Diese Methode erstellt ein neues {@link Item} im {@link Item#CREATE_STATE} und gibt dieses zurück. Sie wird bei {@link Pool#create()} aufgerufen.
+	 * 
+	 * @see Item#state()
+	 * @see Pool#create()
+	 * @return neues {@link Item}.
+	 */
+	protected abstract GItem doCreate();
+
+	/**
+	 * Diese Methode wird bei {@link Item#append()} zur Zustandsüberführung aufgerufen.
+	 * 
+	 * @param item {@link Item}.
+	 */
+	protected abstract void doAppend(final GItem item);
+
+	/**
+	 * Diese Methode wird bei {@link Item#remove()} zur Zustandsüberführung aufgerufen.
+	 * 
+	 * @param item {@link Item}.
+	 */
+	protected abstract void doRemove(final GItem item);
+
+	/**
+	 * Diese Methode wird bei {@link Item#update()} zur Zustandsüberführung aufgerufen.
+	 * 
+	 * @param item {@link Item}.
+	 */
+	protected abstract void doUpdate(final GItem item);
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see #find(Field, Object, int)
+	 */
+	@Override
+	public <GValue> GItem find(final Field<? super GItem, ? extends GValue> field, final GValue value) throws NullPointerException {
+		return this.find(field, value, Item.APPEND_STATE);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see #items()
+	 * @see FilteredSelection
+	 */
+	@Override
+	public <GValue> GItem find(final Field<? super GItem, ? extends GValue> field, final GValue value, final int states) throws NullPointerException {
+		return new FilteredSelection<GItem>(this.items(states)).find(field, value);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see #findAll(Field, Object, int)
+	 */
+	@Override
+	public <GValue> Selection<GItem> findAll(final Field<? super GItem, ? extends GValue> field, final GValue value) throws NullPointerException {
+		return this.findAll(field, value, Item.APPEND_STATE);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * 
+	 * @see #items()
+	 * @see FilteredSelection
+	 */
+	@Override
+	public <GValue> Selection<GItem> findAll(final Field<? super GItem, ? extends GValue> field, final GValue value, final int states)
+		throws NullPointerException {
+		return new FilteredSelection<GItem>(this.items(states)).findAll(field, value);
+	}
+
+	/**
+	 * {@inheritDoc} Diese entspricht der des {@link #type()}s.
+	 */
+	@Override
+	public String label() {
+		return this.type().label();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int size() {
+		return this.items().size();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Collection<? extends GItem> items() {
+		return this.items(Item.APPEND_STATE);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public GItem create() {
+		return this.doCreate();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Iterator<GItem> iterator() {
+		return Iterators.iterator(this.items().iterator());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.owner());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean equals(final Object object) {
+		if(object == this) return true;
+		if(!(object instanceof Pool<?>)) return false;
+		final Pool<?> data = (Pool<?>)object;
+		return Objects.equals(this.type(), data.type()) && Objects.equals(this.owner(), data.owner());
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public String toString() {
+		return Objects.toStringCall(this);
+	}
+
+}
