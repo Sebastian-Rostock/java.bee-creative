09b1623fcb35b8f8e1e1164fd54c41d5ba2f08d9
Sebastian Rostock <bee-creative@gmx.de>
2019-03-03 20:26:16 +0100

    new feature logger + result completed

--- src/main/java/bee/creative/log/Logger.java
+++ src/main/java/bee/creative/log/Logger.java
@@ -3,35 +3,33 @@ package bee.creative.log;
 import bee.creative.util.Objects;
 import bee.creative.util.Strings;
 
-/** Diese Klasse dient der Erfassung hierarchischer Protokollzeilen. Diese Meldungen werden in der {@link #toStrings() Textdarstellung} innerhalb von
- * Protokollebenen entsprechend eingerückt dargestellt.
+/** Diese Klasse dient der Erfassung hierarchischer Protokollzeilen. Diese werden in der {@link #toStrings() Textdarstellung} innerhalb von Protokollebenen
+ * entsprechend eingerückt dargestellt.
  *
  * @author [cc-by] 2019 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/] */
 public class Logger {
 
 	/** Dieses Feld speichert den Ring der erfassten Protokollzeilen. */
-	final LoggerNode head = new LoggerNode(null, null);
+	private final LoggerNode head = new LoggerNode(null, null);
 
-	/** Diese Methode öffnet eine neue Protokollebene, wodurch alle danach erfassten Protokollzeilen um eins weiter eingerückt werden.
-	 *
-	 * @see LoggerResult#toIndent(int) */
+	/** Diese Methode ist eine Abkürzung für {@link #openScope(Object) this.openScope(null)}. */
 	public void openScope() {
-		this.openScopeImpl(null, null);
+		this.openScope(null);
 	}
 
-	/** Diese Methode öffnet eine neue Protokollebene mit der gegebenen Kopfzeile, wodurch alle danach erfassten Protokollzeilen um eins weiter eingerückt werden.
+	/** Diese Methode öffnet eine neue Protokollebene mit der gegebenen Protokollzeilen als Kopfzeile, wodurch alle danach erfassten Protokollzeilen um eins
+	 * weiter eingerückt werden.
 	 *
 	 * @see LoggerResult#push(Object)
-	 * @see LoggerResult#toIndent(int)
 	 * @param text Kopfzeile oder {@code null}. */
 	public void openScope(final Object text) {
 		this.openScopeImpl(text, null);
 	}
 
-	/** Diese Methode öffnet eine neue Protokollebene mit der gegebenen Kopfzeile, wodurch alle danach erfassten Protokollzeilen um eins weiter eingerückt werden.
+	/** Diese Methode öffnet eine neue Protokollebene mit der gegebenen Protokollzeilen als Kopfzeile, wodurch alle danach erfassten Protokollzeilen um eins
+	 * weiter eingerückt werden.
 	 *
 	 * @see LoggerResult#push(Object, Object[])
-	 * @see LoggerResult#toIndent(int)
 	 * @param text Formattext der Kopfzeile oder {@code null}.
 	 * @param args Formatargumente oder Textbausteine der Kopfzeile.
 	 * @throws NullPointerException Wenn {@code args} {@code null} ist. */
@@ -44,29 +42,26 @@ public class Logger {
 		this.head.pushOpen(text, args);
 	}
 
-	/** Diese Methode verlässt die aktuelle Protokollebene, wodurch alle danach erfassten Protokollzeilen um eins weniger eingerückt werden. Sie entfernt diese
-	 * Protokollebene nur dann, wenn sie leer ist, d.h. wenn in ihr keine Protokollzeilen erfasst wurden.
-	 *
-	 * @see LoggerResult#toIndent(int) */
+	/** Diese Methode ist eine Abkürzung für {@link #closeScope(Object) this.closeScope(null)}. */
 	public void closeScope() {
-		this.closeScopeImpl(null, null);
+		this.closeScope(null);
 	}
 
-	/** Diese Methode verlässt die aktuelle Protokollebene mit der gegebenen Fußzeile, wodurch alle danach erfassten Protokollzeilen um eins weniger eingerückt
-	 * werden. Sie entfernt diese Protokollebene nur dann, wenn sie leer ist, d.h. wenn in ihr keine Protokollzeilen erfasst wurden.
+	/** Diese Methode verlässt die aktuelle Protokollebene mit der gegebenen Protokollzeilen als Fußzeile, wodurch alle danach erfassten Protokollzeilen um eins
+	 * weniger eingerückt werden. Sie entfernt die Protokollebene zusammen mit ihrer Kopf- und Fußzeile nur dann, wenn in der Protokollebene keine weiteren
+	 * Protokollzeilen erfasst wurden.
 	 *
 	 * @see LoggerResult#push(Object)
-	 * @see LoggerResult#toIndent(int)
 	 * @param text Fußzeile oder {@code null}. */
 	public void closeScope(final Object text) {
 		this.closeScopeImpl(text, null);
 	}
 
-	/** Diese Methode verlässt die aktuelle Protokollebene mit der gegebenen Fußzeile, wodurch alle danach erfassten Protokollzeilen um eins weniger eingerückt
-	 * werden. Sie entfernt diese Protokollebene nur dann, wenn sie leer ist, d.h. wenn in ihr keine Protokollzeilen erfasst wurden.
+	/** Diese Methode verlässt die aktuelle Protokollebene mit der gegebenen Protokollzeilen als Fußzeile, wodurch alle danach erfassten Protokollzeilen um eins
+	 * weniger eingerückt werden. Sie entfernt die Protokollebene zusammen mit ihrer Kopf- und Fußzeile nur dann, wenn in der Protokollebene keine weiteren
+	 * Protokollzeilen erfasst wurden.
 	 *
 	 * @see LoggerResult#push(Object, Object[])
-	 * @see LoggerResult#toIndent(int)
 	 * @param text Formattext der Fußzeile oder {@code null}.
 	 * @param args Formatargumente oder Textbausteine der Fußzeile.
 	 * @throws NullPointerException Wenn {@code args} {@code null} ist. */
@@ -109,38 +104,44 @@ public class Logger {
 	}
 
 	/** Diese Methode erfasst die gegebene Protokollzeile in der aktuellen Protokollebene als Kopfzeile der gegebenen Fehlerursache. Die Protokollzeile zur
-	 * Fehlerursache wird danach um eins weiter eingerückt über {@link #pushEntry(Object)} erfasst. Wenn die gegebene Protokollzeile {@code null} ist, wird die
-	 * Protokollzeile der Fehlerursache nicht weiter eingerückt.
+	 * Fehlerursache wird danach um eins weiter eingerückt über {@link #pushEntry(Object)} erfasst.
 	 *
 	 * @see LoggerResult#push(Object)
-	 * @param cause Fehlerursache.
+	 * @param cause Fehlerursache oder {@code null}.
 	 * @param text Protokollzeile oder {@code null}. */
-	public void pushEntry(final Throwable cause, final Object text) {
-		this.pushEntryImpl(cause, text, null);
+	public void pushError(final Throwable cause, final Object text) {
+		this.pushErrorImpl(cause, text, null);
 	}
 
 	/** Diese Methode erfasst die gegebene Protokollzeile in der aktuellen Protokollebene als Kopfzeile der gegebenen Fehlerursache. Die Protokollzeile zur
 	 * Fehlerursache wird danach um eins weiter eingerückt über {@link #pushEntry(Object)} erfasst.
 	 *
 	 * @see LoggerResult#push(Object, Object[])
-	 * @param cause Fehlerursache.
+	 * @param cause Fehlerursache oder {@code null}.
 	 * @param text Formattext der Protokollzeile oder {@code null}.
 	 * @param args Formatargumente oder Textbausteine der Protokollzeile.
 	 * @throws NullPointerException Wenn {@code args} {@code null} ist. */
-	public void pushEntry(final Throwable cause, final String text, final Object... args) throws NullPointerException {
-		this.pushEntryImpl(cause, text, Objects.notNull(args));
+	public void pushError(final Throwable cause, final String text, final Object... args) throws NullPointerException {
+		this.pushErrorImpl(cause, text, Objects.notNull(args));
 	}
 
 	@SuppressWarnings ("javadoc")
-	void pushEntryImpl(final Throwable cause, final Object text, final Object[] args) {
+	void pushErrorImpl(final Throwable cause, final Object text, final Object[] args) {
 		if (cause == null) {
 			this.pushEntryImpl(text, args);
 		} else if ((text == null) && (args == null)) {
 			this.pushEntryImpl(cause, null);
 		} else {
-			this.openScopeImpl(text, args);
+			this.head.pushOpen(text, args);
 			this.head.pushClose(cause, null);
 		}
+	}
+
+	/** Diese Methode erfasst die gegebenen Protokollzeilen in der aktuellen Protokollebene.
+	 *
+	 * @param logger Protokollzeilen oder {@code null}. */
+	public void pushLogger(final Logger logger) {
+		this.pushEntryImpl(logger, null);
 	}
 
 	/** Diese Methode entfernt alle bisher erfassten Protokollzeilen. */
@@ -162,16 +163,21 @@ public class Logger {
 	 * @see #toStrings(LoggerResult)
 	 * @return Textdarstellungen. */
 	public String[] toStrings() {
-		final LoggerResult result = new LoggerResult();
-		this.toStrings(result);
-		return result.get();
+		return this.toStrings(new LoggerResult());
 	}
 
 	/** Diese Methode gibt die Textdarstellungen aller erfassten Protokollzeilen zurück. Diese werden über den gegebenen {@link LoggerResult} formatiert.
 	 *
-	 * @param result Generator der Textdarstellungen. */
-	public void toStrings(final LoggerResult result) throws NullPointerException {
-		Objects.notNull(result);
+	 * @param result Generator der Textdarstellungen.
+	 * @return Textdarstellungen.
+	 * @throws NullPointerException Wenn {@code result} {@code null} ist. */
+	public String[] toStrings(final LoggerResult result) throws NullPointerException {
+		toStringImpl(result);
+		return result.get();
+	}
+
+	@SuppressWarnings ("javadoc")
+	void toStringImpl(final LoggerResult result) throws NullPointerException {
 		final LoggerNode prev = this.head;
 		for (LoggerNode next = prev.next; prev != next; next = next.next) {
 			result.pushImpl(next);
--- src/main/java/bee/creative/log/LoggerResult.java
+++ src/main/java/bee/creative/log/LoggerResult.java
@@ -2,63 +2,76 @@ package bee.creative.log;
 
 import java.util.ArrayList;
 import java.util.List;
+import bee.creative.util.Integers;
 import bee.creative.util.Producer;
 import bee.creative.util.Strings;
 
+/** Diese Klasse implementiert den Generator der Textdarstellungen eines {@link Logger}.
+ *
+ * @author [cc-by] 2019 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/] */
 public class LoggerResult implements Producer<String[]> {
 
-	/** Dieses Feld speichert die erfassten Protokolleinträge. */
+	/** Dieses Feld speichert die erfassten Protokollzeilen. */
 	private final List<String> result = new ArrayList<>();
 
 	/** Dieses Feld speichert die Anzahl der Protokollebenen. */
 	private int indent;
 
 	/** Dieses Feld puffert die Zeichenketten zur Einrückung für {@link #toIndent(int)}. */
-	private final String[] indents = new String[1000];
+	private final String[] indents = new String[100];
 
+	/** Diese Methode gibt die Textdarstellungen der {@link #push(String) erfassten} Protokollzeilen zurück.
+	 *
+	 * @return Textdarstellungen. */
 	@Override
 	public String[] get() {
 		return this.result.toArray(new String[this.result.size()]);
 	}
 
-	/** Diese Methode erfasst die gegebene Zeichenkette mit der aktuellen Einrückung als Protokolleintrag nur dann, wenn diese Zeichenkette nicht {@code null} und
-	 * nicht leer ist.
+	/** Diese Methode erfasst die gegebene Zeichenkette mit der aktuellen Einrückung als Protokollzeile nur dann, wenn sie nicht {@code null} ist. Die Einrückung
+	 * wird dazu über {@link #toIndent(int)} ermittelt.
 	 *
 	 * @param text Zeichenkette oder {@code null}. */
 	public void push(final String text) {
-		if ((text == null) || text.isEmpty()) return;
+		if (text == null) return;
 		final String indent = this.toIndent(this.indent);
 		this.result.add(indent.concat(text));
 	}
 
-	/** Diese Methode ist eine Abkürzung für {@link #push(String) this.push(this.toText(text))}. */
-	public void push(final Object text) {
-		this.push(this.toText(text));
+	/** Diese Methode ist eine Abkürzung für {@link #push(Object, Object[]) this.push(text, null)}. */
+	public void push(final Object object) {
+		this.push(object, null);
 	}
 
-	/** Diese Methode überführt die gegebenen Parameter in eine Zeichenkette und delegiert diese an {@link #push(String)}.
+	/** Diese Methode erfasst die gegebene Protokollzeile.
 	 * <ul>
-	 * <li>Wenn die Objektliste {@code args} {@code null} ist, wird die Zeichenkette über {@link #toText(Object) this.toText(text)} ermittelt. Solche Zeilen
-	 * werden durch {@link Logger#openScope()}, {@link Logger#openScope(Object)}, {@link Logger#pushEntry(Object)}, {@link Logger#pushEntry(Throwable, Object)},
-	 * {@link Logger#closeScope()} und {@link Logger#closeScope(Object)} bereitgestellt.</li>
-	 * <li>Wenn das Objekt {@code text} {@code null} ist, wird die Zeichenkette über {@link Strings#join(Object[]) Strings.join(this.toArgs(args))} berechnet.
-	 * Solche Zeilen können durch {@link Logger#openScope(String, Object...)}, {@link Logger#pushEntry(String, Object...)},
-	 * {@link Logger#pushEntry(Throwable, String, Object...)} und {@link Logger#closeScope(String, Object...)} erzeugt werden, wenn als Formattext {@code null}
+	 * <li>Wenn das Objekt {@code text} ein {@link Logger} ist, werden dessen Protokollzeilen rekursiv erfasst. Die Rekursion ist nicht gegen Endlosschleifen
+	 * abgesichert.</li>
+	 * <li>Wenn die Objektliste {@code args} {@code null} ist, wird die Protokollzeile über {@link #push(Object) this.push(toString(text))} erfasst.<br>
+	 * Solche Protokollzeilen werden durch {@link Logger#openScope()}, {@link Logger#openScope(Object)}, {@link Logger#pushEntry(Object)},
+	 * {@link Logger#pushError(Throwable, Object)}, {@link Logger#closeScope()} und {@link Logger#closeScope(Object)} bereitgestellt.</li>
+	 * <li>Wenn das Objekt {@code text} {@code null} ist, wird die über {@link Strings#join(Object[]) Strings.join(this.toObjects(args))} erzeugte Zeichenkette
+	 * über {@link #push(String)} erfast.<br>
+	 * Solche Protokollzeilen können durch {@link Logger#openScope(String, Object...)}, {@link Logger#pushEntry(String, Object...)},
+	 * {@link Logger#pushError(Throwable, String, Object...)} und {@link Logger#closeScope(String, Object...)} erzeugt werden, wenn als Formattext {@code null}
 	 * eingesetzt wird.</li>
-	 * <li>Andernfalls wird die Zeichenkette über {@link String#format(String, Object...) String.format(this.toText(text), this.toArgs(args)))} erzeugt.</li>
+	 * <li>Andernfalls wird die über {@link String#format(String, Object...) String.format(this.toString(text), this.toObjects(args)))} erzeugte Zeichenkette über
+	 * {@link #push(String)} erfasst.</li>
 	 * </ul>
 	 *
-	 * @see #toText(Object)
-	 * @see #toArgs(Object[])
-	 * @param text Textbaustein, Formattext oder {@code null}.
+	 * @see #toString(Object)
+	 * @see #toObjects(Object[])
+	 * @param text {@link Logger}, Textbaustein, Formattext oder {@code null}.
 	 * @param args Textbausteine, Formatargumente oder {@code null}. */
 	public void push(final Object text, final Object[] args) {
-		if (args == null) {
-			this.push(this.toText(text));
+		if (text instanceof Logger) {
+			((Logger)text).toStringImpl(this);
+		} else if (args == null) {
+			this.push(this.toString(text));
 		} else if (text == null) {
-			this.push(Strings.join(this.toArgs(args)));
+			this.push(Strings.join(this.toObjects(args)));
 		} else {
-			this.push(String.format(this.toText(text), this.toArgs(args)));
+			this.push(String.format(this.toString(text), this.toObjects(args)));
 		}
 	}
 
@@ -73,68 +86,67 @@ public class LoggerResult implements Producer<String[]> {
 	}
 
 	/** Diese Methode gibt die Zeichenkette zum gegebenen Objekt zurück. Sie wird in {@link #push(Object)} und {@link #push(Object, Object[])} zur Ermittlung des
-	 * Textbausteins bzw. Formattexts einer Protokollzeile verwendet. Nachfahren sollten diese überschreiben, wenn bspw. für {@link Throwable} eine bessere
-	 * Zeichenkette ermittelt werden soll, als deren {@link Object#toString() Textdarstellung}.
+	 * Textbausteins bzw. Formattexts einer Protokollzeile verwendet. Nachfahren sollten diese Methode überschreiben, wenn bspw. für {@link Throwable} oder andere
+	 * besondere Objekte eine bessere Zeichenkette ermittelt werden soll, als deren einfache {@link Object#toString() Textdarstellung}. Wenn das gegebene Objekt
+	 * {@code null} ist, wird {@code null} geliefert.
 	 *
-	 * @param text Objekt oder {@code null}.
+	 * @param object Objekt oder {@code null}.
 	 * @return Zeichenkette oder {@code null}. */
-	public String toText(final Object text) {
-		if (text == null) return null;
-		return text.toString();
+	public String toString(final Object object) {
+		if (object == null) return null;
+		return object.toString();
 	}
 
-	/** Diese Methode TODO
-	 * <p>
-	 * Nachfahren sollten diese überschreiben, wenn Textbausteine bzw. Formatargumente eine bessere Zeichenkette ermittelt werden soll, als deren
-	 * {@link Object#toString() Textdarstellung}.
+	/** Diese Methode gibt die Zeichenkette zur Einrückung um die gegebene Anzahl an Protokollebenen zurück. Die ersten {@code 100} Zeichenkette werden zur
+	 * schnellen Wiederverwendung gepuffert. Eine negative Anzahl wird wie {@code 0} behandelt. Nachfahren können die Berechnung der Zeichenkette in
+	 * {@link #customIndent(int)} anpassen.
 	 *
-	 * @param text Objekt.
-	 * @return Textbaustein bzw. Formatargument. */
-	public Object toArg(final Object text) {
-		return text;
-	}
-
-	/** Diese Methode wandelt die gegebene Objektliste elementweise über {@link #toArg(Object)} in eine neue Objektliste um und gibt diese zurück. Sie wird von
-	 * {@link #push(Object, Object[])} zur anpassung der Textbausteine bzw. Formatargumente verwendet.
-	 *
-	 * @param args Objektliste.
-	 * @return Textbausteine bzw. Formatargumente.
-	 * @throws NullPointerException Wenn {@code args} {@code null} ist. */
-	public Object[] toArgs(final Object[] args) throws NullPointerException {
-		final int length = args.length;
-		if (length == 0) return args;
-		final Object[] result = new Object[length];
-		for (int i = 0; i < length; i++) {
-			result[i] = this.toArg(args[i]);
-		}
+	 * @param count Anzahl der Protokollebenen.
+	 * @return Zeichenkette zur Einrückung. */
+	public String toIndent(final int count) {
+		if (count < 0) return this.toIndent(0);
+		final String[] indents = this.indents;
+		if (count >= indents.length) return this.customIndent(count);
+		String result = indents[count];
+		if (result != null) return result;
+		indents[count] = (result = this.customIndent(count));
 		return result;
 	}
 
-	/** Diese Methode gibt die Zeichenkette zur Einrückung um die gegebene Anzahl der Protokollebenen zurück. Die ersten 1000 werden zur schnellen
-	 * Wiederverwendung gepuffert. Nachfahren können die Berechnung der Zeichenkette in {@link #customIndent(int)} anpassen. Eine negative Anzahl wird wie 0
-	 * behandelt.
+	/** Diese Methode gibt die Textbausteine bzw. Formatargumente zur gegebenen Objektliste zurück. Sie wird von {@link #push(Object, Object[])} zur Anpassung der
+	 * Textbausteine bzw. Formatargumente einer Protokollzeile verwendet. Dies kann notwendig werden, wenn für besondere Objekte ein bessere Textbaustein
+	 * ermittelt werden soll, als die seiner {@link Object#toString() Textdarstellung}. Die gegebene Objektliste wird dazu elementweise über
+	 * {@link #customObject(Object)} umgewandelt.
 	 *
-	 * @param indent Anzahl der Protokollebenen.
-	 * @return Zeichenkette zur Einrückung. */
-	public String toIndent(final int indent) {
-		if (indent < 0) return this.toIndent(0);
-		final String[] indents = this.indents;
-		if (indent >= indents.length) return this.customIndent(indent);
-		String result = indents[indent];
-		if (result != null) return result;
-		result = this.customIndent(indent);
-		indents[indent] = result;
+	 * @param objects Objektliste.
+	 * @return Textbausteine bzw. Formatargumente.
+	 * @throws NullPointerException Wenn {@code args} {@code null} ist. */
+	public Object[] toObjects(final Object[] objects) throws NullPointerException {
+		final int length = objects.length;
+		if (length == 0) return objects;
+		final Object[] result = new Object[length];
+		for (int i = 0; i < length; i++) {
+			result[i] = this.customObject(objects[i]);
+		}
 		return result;
 	}
 
 	/** Diese Methode liefert die Zeichenkette zur Einrückung um die gegebene Anzahl der Protokollebenen.
 	 *
-	 * @param indent Anzahl der Protokollebenen.
+	 * @param count Anzahl der Protokollebenen.
 	 * @return Zeichenkette zur Einrückung. */
-	protected String customIndent(final int indent) {
-		final String space = ")                                        "; // 40 x Space
-		if (indent <= 20) return space.substring(1, indent + indent + 1);
-		return "(" + indent + space.substring(0, 40);
+	protected String customIndent(final int count) {
+		final String space = "                                        "; // 40 x Space
+		if (count <= 20) return space.substring(0, count << 1);
+		return "(" + count + ")" + space.substring(0, 40 - Integers.stringSize(count));
+	}
+
+	/** Diese Methode ird in {@link #toObjects(Object[])} zur Anpassung von Textbausteinen bzw. Formatargumenten eingesetzt.
+	 *
+	 * @param object Objekt.
+	 * @return Textbaustein bzw. Formatargument. */
+	protected Object customObject(final Object object) {
+		return object;
 	}
 
 }
