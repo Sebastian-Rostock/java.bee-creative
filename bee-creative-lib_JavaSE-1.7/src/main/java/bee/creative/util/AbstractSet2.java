c43fd9a11405ff85b49227e234c0b66029a5e0b5
Sebastian Rostock <bee-creative@gmx.de>
2023-02-25 19:30:49 +0100

    new abstract collection classes with iterator2

---
+++ bee-creative-lib_JavaSE-1.7/src/main/java/bee/creative/util/AbstractSet2.java
@@ -0,0 +1,10 @@
+package bee.creative.util;
+
+import java.util.AbstractSet;
+
+/** Diese Klasse implementiert ein {@link AbstractSet} als {@link Set2}.
+ *
+ * @author [cc-by] 2023 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/] */
+public abstract class AbstractSet2<E> extends AbstractSet<E> implements Set2<E> {
+
+}
