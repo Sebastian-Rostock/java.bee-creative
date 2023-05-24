88ec2565261f90127745a12a99e791b288d34165
Sebastian Rostock <bee-creative@gmx.de>
2023-05-24 22:14:01 +0200

    qs base set api refined

---
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/QISet.java
@@ -0,0 +1,45 @@
+package bee.creative.qs;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import bee.creative.util.Iterator2;
+
+/** Diese Schnittstelle definiert eine beliebig große Sicht auf eine Menge von Objekten mit Bezug zu einem {@link #owner() Graphspeicher}.
+ *
+ * @author [cc-by] 2023 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/]
+ * @param <GI> Typ der Einträge. */
+public interface QISet<GI> extends QO, Iterable<GI> {
+
+	/** Diese Methode gibt die Anzahl der in dieser Menge enthaltenen Objekte zurück.
+	 *
+	 * @return Objektanzahl. */
+	public long size();
+
+	/** Diese Methode gibt nur dann {@code true} zurück, wenn diese Menge leer ist.
+	 *
+	 * @return {@code true}, nur wenn der {@link #iterator()} kein Objekt liefert. */
+	public boolean isEmpty();
+
+	/** Diese Methode gibt das erste Objekt in dieser Menge oder {@code null} zurück.
+	 *
+	 * @return erstes Objekt oder {@code null}. */
+	public GI first();
+
+	/** Diese Methode gibt den {@link Iterator2} über die Objekte dieser Menge zurück. Die Methode {@link Iterator#remove()} wird nicht unterstützt.
+	 *
+	 * @return Iterator. */
+	@Override
+	public Iterator2<GI> iterator();
+
+	/** Diese Methode gibt eine Kopie dieser Menge als {@link Set} zurück.
+	 *
+	 * @return Kopie dieser Menge. */
+	public Set<GI> toSet();
+
+	/** Diese Methode gibt eine Kopie dieser Menge als {@link List} zurück.
+	 *
+	 * @return geordnete Kopie dieser Menge. */
+	public List<GI> toList();
+
+}
--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/QOSet.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/QOSet.java
@@ -1,32 +1,14 @@
 package bee.creative.qs;
 
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
 import bee.creative.util.Filter;
-import bee.creative.util.Iterator2;
 
-/** Diese Schnittstelle definiert eine beliebig große Sicht auf eine Menge von Objekten mit Bezug zu einem {@link #owner() Graphspeicher}.
+/** Diese Schnittstelle definiert eine beliebig große Sicht auf eine filter- und kopier- und komponierbare Menge von Objekten mit Bezug zu einem {@link #owner()
+ * Graphspeicher}.
  *
  * @author [cc-by] 2020 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/]
  * @param <GI> Typ der Einträge.
  * @param <GISet> Typ dieser Menge. */
-public interface QOSet<GI, GISet> extends QO, Iterable<GI> {
-
-	/** Diese Methode gibt die Anzahl der in dieser Menge enthaltenen Objekte zurück.
-	 *
-	 * @return Objektanzahl. */
-	public long size();
-
-	/** Diese Methode gibt nur dann {@code true} zurück, wenn diese Menge leer ist.
-	 *
-	 * @return {@code true}, nur wenn der {@link #iterator()} kein Objekt liefert. */
-	public boolean isEmpty();
-
-	/** Diese Methode gibt das erste Objekt in dieser Menge oder {@code null} zurück.
-	 *
-	 * @return erstes Objekt oder {@code null}. */
-	public GI first();
+public interface QOSet<GI, GISet> extends QISet<GI> {
 
 	/** Diese Methode gibt eine temporäre Kopie dieser Menge zurück.
 	 *
@@ -61,21 +43,5 @@ public interface QOSet<GI, GISet> extends QO, Iterable<GI> {
 	 * @param set Menge.
 	 * @return Schitt dieser mit der gegebenen Menge. */
 	public GISet intersect(GISet set) throws NullPointerException, IllegalArgumentException;
-
-	/** Diese Methode gibt den {@link Iterator2} über die Objekte dieser Menge zurück. Die Methode {@link Iterator#remove()} wird nicht unterstützt.
-	 *
-	 * @return Iterator. */
-	@Override
-	public Iterator2<GI> iterator();
-
-	/** Diese Methode gibt eine Kopie dieser Menge als {@link Set} zurück.
-	 *
-	 * @return Kopie dieser Menge. */
-	public Set<GI> toSet();
-
-	/** Diese Methode gibt eine {@link #order() geordnete} Kopie dieser Menge als {@link List} zurück.
-	 *
-	 * @return geordnete Kopie dieser Menge. */
-	public List<GI> toList();
 
 }
