fbbac2f8e64230a18fe365fa8a20c5b8ddea03b4
Sebastian Rostock <bee-creative@gmx.de>
2023-05-30 22:25:20 +0200

    h2 qs cache bag for fem integer continued

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/H2QDBag.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/H2QDBag.java
@@ -34,12 +34,13 @@ public abstract class H2QDBag<GI> extends H2QISet<GI> {
 	}
 
 	/** Dieser Konstruktor initialisiert {@link #owner Graphspeicher} und {@link #table Tabelle}. Die Tabelle muss mit der Spalte
-	 * {@code N BIGINT NOT NULL PRIMARY KEY} beginnen. Der Cache besteht nur aus dieser Spalte, gibt die Menge der geprüften {@link QS#nodes() Hperknoten mit
-	 * Textwert} an und ist nur bei der Hauptwertesammlung anzugeben. */
-	protected H2QDBag(final H2QS owner, final H2QQ table, final String cache) {
+	 * {@code N BIGINT NOT NULL PRIMARY KEY} beginnen. Der {@code index} gibt den Namen der automatisch erzeugten Tabelle zur Erfassung der indizierten
+	 * {@link QS#nodes() Hperknoten mit Textwert}. */
+	protected H2QDBag(final H2QS owner, final H2QQ table, final String index) {
 		super(owner, Objects.notNull(table));
-		if (cache == null) return;
-		this.table.push(new Cache(cache));
+		if (index == null) return;
+		new H2QQ().push("CREATE TABLE IF NOT EXISTS ").push(index).push(" (N BIGINT NOT NULL, PRIMARY KEY (N));").update(owner);
+		this.table.push(new Cache(index));
 	}
 
 	protected QN node(final ResultSet next) throws SQLException {
@@ -47,28 +48,36 @@ public abstract class H2QDBag<GI> extends H2QISet<GI> {
 	}
 
 	protected void putItems(final PutItemSet putItemSet) throws SQLException {
+		System.err.println(putItemSet.toList());
 	}
 
 	protected void popItems(final PopItemSet popItemSet) throws SQLException {
+		System.err.println(popItemSet.toList());
 	}
 
 	protected class Cache {
 
 		@Override
 		public String toString() {
-			final Object putValueMark = H2QDBag.this.owner.putValueMark;
-			final Object popValueMark = H2QDBag.this.owner.popValueMark;
-			final boolean putValueChanged = this.putValueMark != putValueMark;
-			final boolean popValueChanged = this.popValueMark != popValueMark;
+			final var that = H2QDBag.this;
+			final var owner = that.owner;
+			final var putValueMark = owner.putValueMark;
+			final var popValueMark = owner.popValueMark;
+			final var putValueChanged = this.putValueMark != putValueMark;
+			final var popValueChanged = this.popValueMark != popValueMark;
 			if (!putValueChanged && !popValueChanged) return "";
 			this.putValueMark = putValueMark;
 			this.popValueMark = popValueMark;
 			try {
 				if (popValueChanged) {
-					H2QDBag.this.popItems(this.popItemSet);
+					final var popItemSet = new PopItemSet(owner, this.index);
+					new H2QQ().push("DELETE FROM ").push(this.index).push(" WHERE N IN (").push(popItemSet).push(")").update(owner);
+					that.popItems(popItemSet);
 				}
 				if (putValueChanged) {
-					H2QDBag.this.putItems(this.putItemSet);
+					final var putItemSet = new PutItemSet(owner, this.index);
+					new H2QQ().push("MERGE INTO ").push(this.index).push(" SELECT N FROM (").push(putItemSet).push(")").update(owner);
+					that.putItems(putItemSet);
 				}
 			} catch (final Exception cause) {
 				throw new IllegalStateException(cause);
@@ -76,37 +85,47 @@ public abstract class H2QDBag<GI> extends H2QISet<GI> {
 			return "";
 		}
 
-		final String cache;
-
-		final PutItemSet putItemSet;
-
-		final PopItemSet popItemSet;
+		final String index;
 
 		Object putValueMark;
 
 		Object popValueMark;
 
-		Cache(final String cache) {
-			this.cache = cache;
-			this.putItemSet = new PutItemSet(H2QDBag.this.owner, cache);
-			this.popItemSet = new PopItemSet(H2QDBag.this.owner, cache);
+		Cache(final String index) {
+			this.index = index;
 		}
 
 	}
 
+	/** Diese Klasse implementiert eine temporäre Kopie der Menge der noch nicht indizierten {@link H2QS#nodes() Hyperknoten mit Textwert} für
+	 * {@link H2QDBag#putItems(PutItemSet)}. Die {@link Entry#getKey() Schüssel} jedes Eintrags nennt die {@link H2QN#key Kennung} des {@link H2QE Hyperknoten},
+	 * dessen {@link H2QN#value() Textwert} im {@link Entry#getValue() Werte} des Eintrags angegeben ist. */
 	protected static class PutItemSet extends H2QISet<Entry<Long, String>> {
+
+		public void collectAll(final Setter<Long, String> setter) throws SQLException {
+			try (var rset = this.table.select(this.owner)) {
+				while (rset.next()) {
+					setter.set((Long)rset.getObject(1), rset.getString(2));
+				}
+			}
+		}
 
 		@Override
 		protected Entry<Long, String> item(final ResultSet next) throws SQLException {
-			return Entries.from(next.getLong(1), next.getString(2));
+			return Entries.from((Long)next.getObject(1), next.getString(2));
 		}
 
-		PutItemSet(final H2QS owner, final String cache) {
-			super(owner, new H2QQ().push("SELECT N, V FROM QN where N NOT IN (SELECT N FROM (").push(cache).push("))"));
+		PutItemSet(final H2QS owner, final String index) {
+			super(owner, null);
+			new H2QQ().push("CREATE TEMPORARY TABLE ").push(this.table)
+				.push(" (N BIGINT NOT NULL, V VARCHAR(1G) NOT NULL, PRIMARY KEY (N)) AS (SELECT N, V FROM QN where N NOT IN (SELECT N FROM ").push(index).push("))")
+				.update(owner);
 		}
 
 	}
 
+	/** Diese Klasse implementiert eine temporäre Kopie der Menge der indizierten {@link H2QS#nodes() Hyperknoten mit Textwert} für
+	 * {@link H2QDBag#popItems(PopItemSet)}. Jeder Eintrag nennt die {@link H2QN#key Kennung} eines gelöschten {@link H2QE Hyperknoten}. */
 	protected static class PopItemSet extends H2QISet<Long> {
 
 		@Override
@@ -114,8 +133,10 @@ public abstract class H2QDBag<GI> extends H2QISet<GI> {
 			return next.getLong(1);
 		}
 
-		PopItemSet(final H2QS owner, final String cache) {
-			super(owner, new H2QQ().push("SELECT N FROM (").push(cache).push(") where N NOT IN (select N from QN)"));
+		PopItemSet(final H2QS owner, final String index) {
+			super(owner, null);
+			new H2QQ().push("CREATE TEMPORARY TABLE ").push(this.table).push(" (N BIGINT NOT NULL, PRIMARY KEY (N)) AS (SELECT N FROM ").push(index)
+				.push(" WHERE N NOT IN (SELECT N FROM QN))").update(owner);
 		}
 
 	}
--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/fem/H2QDFEMIntegerBag.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/fem/H2QDFEMIntegerBag.java
@@ -2,6 +2,7 @@ package bee.creative.qs.h2.fem;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map.Entry;
 import bee.creative.fem.FEMInteger;
 import bee.creative.qs.h2.H2QDBag;
 import bee.creative.qs.h2.H2QQ;
@@ -10,11 +11,10 @@ import bee.creative.qs.h2.H2QS;
 public class H2QDFEMIntegerBag extends H2QDBag<FEMInteger> {
 
 	public static H2QDFEMIntegerBag from(final H2QS owner) {
-		final var result = new H2QDFEMIntegerBag(owner, new H2QQ().push("SELECT N, V FROM QD_FEMINTEGER_VALUE"), "SELECT N FROM QD_FEMINTEGER_CACHE");
+		final var result = new H2QDFEMIntegerBag(owner, new H2QQ().push("SELECT N, V FROM QD_FEMINTEGER"), "QI_FEMINTEGER");
 		new H2QQ() //
-			.push("CREATE TABLE IF NOT EXISTS QD_FEMINTEGER_CACHE (N BIGINT NOT NULL, PRIMARY KEY (N));") //
-			.push("CREATE TABLE IF NOT EXISTS QD_FEMINTEGER_VALUE (N BIGINT NOT NULL, V BIGINT NOT NULL, PRIMARY KEY (N));") //
-			.push("CREATE INDEX IF NOT EXISTS QD_FEMINTEGER_VALUE_V ON QD_FEMINTEGER_VALUE (V);") //
+			.push("CREATE TABLE IF NOT EXISTS QD_FEMINTEGER (N BIGINT NOT NULL, V BIGINT NOT NULL, PRIMARY KEY (N));") //
+			.push("CREATE INDEX IF NOT EXISTS QD_FEMINTEGER_INDEX_V ON QD_FEMINTEGER (V);") //
 			.update(owner);
 		return result;
 	}
@@ -46,13 +46,22 @@ public class H2QDFEMIntegerBag extends H2QDBag<FEMInteger> {
 
 	@Override
 	protected void putItems(final PutItemSet putItemSet) throws SQLException {
-		System.out.println(putItemSet);
+		try (var stmt = new H2QQ().push("MERGE INTO QD_FEMINTEGER (N, V) VALUES (?, ?)").prepare(this.owner)) {
+			for (final Entry<Long, String> i: putItemSet) {
+				try {
+					stmt.setObject(2, Long.valueOf(i.getValue()));
+					stmt.setObject(1, i.getKey());
+					stmt.addBatch();
+				} catch (final Exception ignore) {}
+			}
+			stmt.executeBatch();
+		}
 		super.putItems(putItemSet);
 	}
 
 	@Override
 	protected void popItems(final PopItemSet popItemSet) throws SQLException {
-		System.out.println(popItemSet);
+		new H2QQ().push("DELETE FROM QD_FEMINTEGER WHERE N IN (").push(popItemSet).push(")").update(this.owner);
 		super.popItems(popItemSet);
 	}
 
