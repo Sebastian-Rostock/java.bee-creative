9d9e83cb6e3ca9d9f1ae1bd53041ddc2cfd661cb
Sebastian Rostock <bee-creative@gmx.de>
2023-06-25 22:21:11 +0200

    default interfaces

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/util/Property2.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/util/Property2.java
@@ -10,32 +10,51 @@ import bee.creative.util.Properties.ObservableProperty;
 public interface Property2<GValue> extends Property<GValue>, Producer2<GValue>, Consumer2<GValue> {
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#setup(Property, Producer) Properties.setup(this, setup)}. */
-	public Property2<GValue> setup(Producer<? extends GValue> setup);
+	default Property2<GValue> setup(final Producer<? extends GValue> setup) {
+		return Properties.setup(this, setup);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#observe(Property) Properties.observe(this)}. */
-	public ObservableProperty<GValue> observe();
+	default ObservableProperty<GValue> observe() {
+		return Properties.observe(this);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#translate(Property, Getter, Getter) Properties.translate(this, transGet, transSet)}. */
-	public <GValue2> Property2<GValue2> translate(Getter<? super GValue, ? extends GValue2> transGet, Getter<? super GValue2, ? extends GValue> transSet);
+	default <GValue2> Property2<GValue2> translate(final Getter<? super GValue, ? extends GValue2> transGet,
+		final Getter<? super GValue2, ? extends GValue> transSet) {
+		return Properties.translate(this, transGet, transSet);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#translate(Property, Translator) Properties.translate(this, trans)}. */
-	public <GValue2> Property2<GValue2> translate(Translator<GValue, GValue2> trans);
+	default <GValue2> Property2<GValue2> translate(final Translator<GValue, GValue2> trans) {
+		return Properties.translate(this, trans);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#synchronize(Property) Properties.synchronize(this)}. */
 	@Override
-	public Property2<GValue> synchronize();
+	default Property2<GValue> synchronize() {
+		return Properties.synchronize(this);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Properties#synchronize(Property, Object) Properties.synchronize(this, mutex)}. */
 	@Override
-	public Property2<GValue> synchronize(Object mutex);
+	default Property2<GValue> synchronize(final Object mutex) {
+		return Properties.synchronize(this, mutex);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Fields#from(Property) Fields.from(this)}. */
-	public Field2<Object, GValue> toField();
+	default Field2<Object, GValue> toField() {
+		return Fields.from(this);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Producers#from(Producer) Producers.from(this)}. */
-	public Producer3<GValue> toProducer();
+	default Producer3<GValue> toProducer() {
+		return Producers.from(this);
+	}
 
 	/** Diese Methode ist eine Abkürzung für {@link Consumers#from(Consumer) Consumers.from(this)}. */
-	public Consumer3<GValue> toConsumer();
+	default Consumer3<GValue> toConsumer() {
+		return Consumers.from(this);
+	}
 
 }
