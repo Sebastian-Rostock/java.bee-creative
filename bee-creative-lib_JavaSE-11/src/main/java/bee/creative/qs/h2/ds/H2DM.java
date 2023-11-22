40f73efafc838352d5a945ac01442f7cbf082035
Sebastian Rostock <bee-creative@gmx.de>
2023-11-22 22:25:01 +0100

    ds api continued

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/ds/H2DM.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/qs/h2/ds/H2DM.java
@@ -1,8 +1,5 @@
 package bee.creative.qs.h2.ds;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 import bee.creative.qs.QN;
 import bee.creative.qs.ds.DE;
 import bee.creative.qs.ds.DH;
@@ -15,10 +12,10 @@ import bee.creative.qs.ds.DT;
 import bee.creative.qs.h2.H2QESet;
 import bee.creative.qs.h2.H2QN;
 import bee.creative.qs.h2.H2QS;
-import bee.creative.util.Filters;
 import bee.creative.util.Getter;
+import bee.creative.util.Getters;
 import bee.creative.util.HashMap2;
-import bee.creative.util.Iterables;
+import bee.creative.util.Hashers;
 import bee.creative.util.Translator2;
 import bee.creative.util.Translators;
 
@@ -61,17 +58,22 @@ public class H2DM implements DM {
 
 	@Override
 	public H2DL getLink(String ident) {
-		if (this.linkMap == null) {
-			this.update();
-		}
+		if (this.linkMap != null) return this.linkMap.get(ident);
+		this.linkMap = new HashMap2<>(100);
+		var edgeList = this.edges().havingObject(this.owner().newNode(DL.IDENT_IsLinkWithIdent)).iterator() //
+			.filter(edge -> edge.subject().equals(edge.predicate())).toList();
+		if (edgeList.isEmpty()) return null;
+		var asError = (Getter<Object, String>)value -> "DL with ident " + value + " is not unique";
+		if (edgeList.size() > 1) throw new IllegalArgumentException(asError.get(DL.IDENT_IsLinkWithIdent));
+		this.setupItemMap(this.linkMap, this.asLink(edgeList.get(0).predicate()), this::asLink, asError);
 		return this.linkMap.get(ident);
 	}
 
 	@Override
 	public H2DT getType(String ident) {
-		if (this.typeMap == null) {
-			this.update();
-		}
+		if (this.typeMap != null) return this.typeMap.get(ident);
+		this.typeMap = new HashMap2<>(100);
+		this.setupItemMap(this.typeMap, this.getLink(DT.IDENT_IsTypeWithIdent), this::asType, value -> "DT with ident " + value + " is not unique");
 		return this.typeMap.get(ident);
 	}
 
@@ -85,42 +87,42 @@ public class H2DM implements DM {
 		return this.typeTrans == null ? this.typeTrans = Translators.from(QN.class, DT.class, this::asType, DT::node).optionalize() : this.typeTrans;
 	}
 
+	public void install() {
+		this.installType(DT.IDENT_IsType, "domain-type");
+		this.installType(DL.IDENT_IsLink, "domain-link");
+
+		this.installLink(DT.IDENT_IsTypeWithIdent, "type-idents", //
+			DT.IDENT_IsType, Handling.Association, Multiplicity.Multiplicity11, null, Handling.Association, Multiplicity.Multiplicity1N);
+		this.installLink(DT.IDENT_IsTypeWithLabel, "type-label", DT.IDENT_IsType, Handling.Association, Multiplicity.Multiplicity1N, null, Handling.Aggregation,
+			Multiplicity.Multiplicity01);
+		this.installLink(DT.IDENT_IsTypeWithInstance, "type-instances", //
+			DT.IDENT_IsType, Handling.Aggregation, Multiplicity.Multiplicity11, null, Handling.Association, Multiplicity.Multiplicity0N);
+		this.installLink(DL.IDENT_IsLinkWithIdent, "link-idents", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity11, null, Handling.Association, Multiplicity.Multiplicity1N);
+		this.installLink(DL.IDENT_IsLinkWithLabel, "link-label", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity1N, null, Handling.Aggregation, Multiplicity.Multiplicity01);
+		this.installLink(DL.IDENT_IsLinkWithSourceType, "link-source-type", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, DT.IDENT_IsType, Handling.Aggregation, Multiplicity.Multiplicity01);
+		this.installLink(DL.IDENT_IsLinkWithSourceHandling, "link-source-handling", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null, Handling.Aggregation, Multiplicity.Multiplicity11);
+		this.installLink(DL.IDENT_IsLinkWithSourceMultiplicity, "link-source-multiplicity", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null, Handling.Aggregation, Multiplicity.Multiplicity11);
+		this.installLink(DL.IDENT_IsLinkWithTargetType, "link-target-type", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, DT.IDENT_IsType, Handling.Aggregation, Multiplicity.Multiplicity01);
+		this.installLink(DL.IDENT_IsLinkWithTargetHandling, "link-target-handling", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null, Handling.Aggregation, Multiplicity.Multiplicity11);
+		this.installLink(DL.IDENT_IsLinkWithTargetMultiplicity, "link-target-multiplicity", //
+			DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null, Handling.Aggregation, Multiplicity.Multiplicity11);
+
+		this.installInstances(DT.IDENT_IsType, this.typeMap.values());
+		this.installInstances(DL.IDENT_IsLink, this.linkMap.values());
+	}
+
 	/** Diese Methode übernimmt Änderungen an {@link DE#idents()} auf die internen Puffer zur Beschleunigung von {@link #getLink(String)} und
 	 * {@link #getType(String)}. Wenn das {@link DL Datenfeld} zu {@link DL#IDENT_IsLinkWithIdent} nicht verwendet wird, wird das Datenmodell initialisiert. Wenn
 	 * essentielle Datenfelder oder Datentypen nicht ermittelt werden können, wird eine Ausnahme ausgelöst. */
-	public void update() {
-		var rollback = true;
-		var linkMapBackup = this.linkMap;
-		var typeMapBackup = this.typeMap;
-		try {
-			this.linkMap = new HashMap2<>(100);
-			this.typeMap = new HashMap2<>(100);
-			var isLinkWithIdent = this.asLink(this.parent.install(DL.IDENT_IsLinkWithIdent));
-			this.setupIdentMap(isLinkWithIdent, this.linkMap, this::asLink, ident -> "DL with ident " + ident + " is not unique");
-			if (this.linkMap.isEmpty()) {
-				this.linkMap.put(DL.IDENT_IsLinkWithIdent, isLinkWithIdent);
-				this.setup();
-			} else {
-				if (!isLinkWithIdent.equals(this.linkMap.get(DL.IDENT_IsLinkWithIdent))) throw new IllegalStateException();
-			}
-			this.checkIdentMap(this.linkMap, ident -> "DL with ident " + ident + " is missing", //
-				DT.IDENT_IsTypeWithIdent, DT.IDENT_IsTypeWithLabel, DT.IDENT_IsTypeWithInstance, //
-				DL.IDENT_IsLinkWithIdent, DL.IDENT_IsLinkWithLabel, //
-				DL.IDENT_IsLinkWithSourceType, DL.IDENT_IsLinkWithSourceHandling, DL.IDENT_IsLinkWithSourceMultiplicity, //
-				DL.IDENT_IsLinkWithTargetType, DL.IDENT_IsLinkWithTargetHandling, DL.IDENT_IsLinkWithTargetMultiplicity);
-			var isTypeWithIdent = this.getLink(DT.IDENT_IsTypeWithIdent);
-			this.setupIdentMap(isTypeWithIdent, this.typeMap, this::asType, ident -> "DT with ident " + ident + " is not unique");
-			this.checkIdentMap(this.typeMap, ident -> "DT with ident " + ident + " is missing", //
-				DT.IDENT_IsType, DL.IDENT_IsLink);
-			this.linkMap.compact();
-			this.typeMap.compact();
-			rollback = false;
-		} finally {
-			if (rollback) {
-				this.linkMap = linkMapBackup;
-				this.typeMap = typeMapBackup;
-			}
-		}
+	public void invalidateIdents() {
+
 	}
 
 	protected H2DL asLink(QN node) {
@@ -131,85 +133,62 @@ public class H2DM implements DM {
 		return new H2DT(this, this.context.owner.asQN(node));
 	}
 
-	protected void setup() {
-		var setup = new Setup();
-		this.setup(setup);
-		setup.install();
-	}
-
-	protected void setup(Setup setup) {
-		setup.putType(DT.IDENT_IsType, "domain-type");
-		setup.putLink(DT.IDENT_IsTypeWithIdent, "type-idents", DT.IDENT_IsType, Handling.Association, Multiplicity.Multiplicity11, null, Handling.Association,
-			Multiplicity.Multiplicity1N);
-		setup.putLink(DT.IDENT_IsTypeWithLabel, "type-label", DT.IDENT_IsType, Handling.Association, Multiplicity.Multiplicity1N, null, Handling.Aggregation,
-			Multiplicity.Multiplicity01);
-		setup.putLink(DT.IDENT_IsTypeWithInstance, "type-instances", DT.IDENT_IsType, Handling.Aggregation, Multiplicity.Multiplicity11, null, Handling.Association,
-			Multiplicity.Multiplicity0N);
-		setup.putType(DL.IDENT_IsLink, "domain-link");
-		setup.putLink(DL.IDENT_IsLinkWithIdent, "link-idents", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity11, null, Handling.Association,
-			Multiplicity.Multiplicity1N);
-		setup.putLink(DL.IDENT_IsLinkWithLabel, "link-label", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity1N, null, Handling.Aggregation,
-			Multiplicity.Multiplicity01);
-		setup.putLink(DL.IDENT_IsLinkWithSourceType, "link-source-type", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, DT.IDENT_IsType,
-			Handling.Aggregation, Multiplicity.Multiplicity01);
-		setup.putLink(DL.IDENT_IsLinkWithSourceHandling, "link-source-handling", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null,
-			Handling.Aggregation, Multiplicity.Multiplicity11);
-		setup.putLink(DL.IDENT_IsLinkWithSourceMultiplicity, "link-source-multiplicity", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null,
-			Handling.Aggregation, Multiplicity.Multiplicity11);
-		setup.putLink(DL.IDENT_IsLinkWithTargetType, "link-target-type", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, DT.IDENT_IsType,
-			Handling.Aggregation, Multiplicity.Multiplicity01);
-		setup.putLink(DL.IDENT_IsLinkWithTargetHandling, "link-target-handling", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null,
-			Handling.Aggregation, Multiplicity.Multiplicity11);
-		setup.putLink(DL.IDENT_IsLinkWithTargetMultiplicity, "link-target-multiplicity", DL.IDENT_IsLink, Handling.Association, Multiplicity.Multiplicity0N, null,
-			Handling.Aggregation, Multiplicity.Multiplicity11);
-	}
-
+	/** Diese Methode liefert den unter dem gegebenen {@link DL#identsAsStrings() Erkennungstextwert} in der {@link #linkMap} hinterlegten {@link DL Datentyp} und
+	 * {@link HashMap2#install(Object, Getter) erzeugt} diesen bei Bedarf, sofern {@code ident} nicht {@code null} ist. Andernfalls wird {@code null}
+	 * geliefert. */
 	protected H2DL installLink(String ident) {
-		return this.linkMap.install(ident, value -> this.asLink(this.context.owner.newNode()));
+		return ident != null ? this.linkMap.install(ident, value -> this.asLink(this.context.owner.newNode())) : null;
 	}
 
-	protected H2QN installLinkNode(String ident) {
-		return this.installLink(ident).node;
+	protected H2DT installType(String ident, String label) {
+		var type = this.installType(ident);
+		this.installTargets(DT.IDENT_IsTypeWithIdent, type).asValueSet().add(ident);
+		this.installTargets(DT.IDENT_IsTypeWithLabel, type).asValue().set(label);
+		return type;
 	}
 
 	protected H2DT installType(String ident) {
-		return this.typeMap.install(ident, value -> this.asType(this.context.owner.newNode()));
+		return ident != null ? this.typeMap.install(ident, value -> this.asType(this.context.owner.newNode())) : null;
 	}
 
-	protected H2QN installTypeNode(String ident) {
-		return this.installType(ident).node;
+	protected H2DL installLink(String ident, String label, String sourceTypeIdent, Handling sourceHandling, Multiplicity sourceMultiplicity,
+		String targetTypeIdent, Handling targetHandling, Multiplicity targetMultiplicity) {
+		var link = this.installLink(ident);
+		this.installTargets(DL.IDENT_IsLinkWithIdent, link).asValueSet().add(ident);
+		this.installTargets(DL.IDENT_IsLinkWithLabel, link).asValue().set(label);
+		this.installTargets(DL.IDENT_IsLinkWithSourceType, link).asNode().translate(this.typeTrans()).set(this.installType(sourceTypeIdent));
+		this.installTargets(DL.IDENT_IsLinkWithSourceHandling, link).asValue().set(Handling.trans.toSource(sourceHandling));
+		this.installTargets(DL.IDENT_IsLinkWithSourceMultiplicity, link).asValue().set(Multiplicity.trans.toSource(sourceMultiplicity));
+		this.installTargets(DL.IDENT_IsLinkWithTargetType, link).asNode().translate(this.typeTrans()).set(this.installType(targetTypeIdent));
+		this.installTargets(DL.IDENT_IsLinkWithTargetHandling, link).asValue().set(Handling.trans.toSource(targetHandling));
+		this.installTargets(DL.IDENT_IsLinkWithTargetMultiplicity, link).asValue().set(Multiplicity.trans.toSource(targetMultiplicity));
+		this.installSources(DT.IDENT_IsTypeWithInstance, link).asNode().translate(this.typeTrans()).set(this.installType(DL.IDENT_IsLink));
+		return link;
 	}
 
-	/** Diese Methode liest die {@link DL#edges() Hyperkanten} des gegebenen {@link DL Datenfelds} ein und bestück damit die gegebenen Abbildungen.
-	 *
-	 * @param <GItem>
-	 * @param isItemWithIdent Datenfeld, dessen {@link DL#edges() Hyperkanten} eingelesen werden.
-	 * @param itemByIdentText
-	 * @param nodeAsItem
-	 * @param errorMessageByIdentNode */
-	protected <GItem> void setupIdentMap(DL isItemWithIdent, Map<? super String, GItem> itemByIdentText, Getter<QN, GItem> nodeAsItem,
-		Getter<QN, String> errorMessageByIdentNode) {
+	protected H2DLTSet installTargets(String linkIdent, H2DE source) {
+		return this.installLink(linkIdent).getTargets(source.node);
+	}
+
+	protected H2DLSSet installSources(String linkIdent, H2DE target) {
+		return this.installLink(linkIdent).getSources(target.node);
+	}
+
+	protected void installInstances(String typeIdent, Iterable<? extends H2DE> instances) {
+		var typeMap = new HashMap2<QN, QN>();
+		typeMap.putAll(instances, H2DE::node, Getters.fromValue(this.installType(typeIdent).node));
+		this.installLink(DT.IDENT_IsTypeWithInstance).setSourceMap(typeMap);
+	}
+
+	protected <GItem> void setupItemMap(HashMap2<String, GItem> itemByIdentMap, H2DL identLink, Getter<QN, GItem> asItem, Getter<Object, String> asError) {
 		var itemNodeByIdentNodeMap = new HashMap2<QN, QN>(100);
-		isItemWithIdent.edges().forEach(edge -> {
-			if (itemNodeByIdentNodeMap.put(edge.object(), edge.subject()) != null) throw new IllegalArgumentException(errorMessageByIdentNode.get(edge.object()));
+		identLink.edges().forEach(edge -> {
+			if (itemNodeByIdentNodeMap.put(edge.object(), edge.subject()) != null) throw new IllegalArgumentException(asError.get(edge.object()));
 		});
-
-		var identTextByIdentNodeMap = new HashMap2<QN, String>(itemNodeByIdentNodeMap.size());
-		isItemWithIdent.owner().newNodes(itemNodeByIdentNodeMap.keySet()).values(identTextByIdentNodeMap::put);
-
-		var itemByItemNodeMap = new HashMap2<QN, GItem>(100);
-		itemNodeByIdentNodeMap.values().iterator().toSet().forEach(itemNode -> itemByItemNodeMap.put(itemNode, nodeAsItem.get(itemNode)));
-
-		itemNodeByIdentNodeMap.forEach((identNode, itemNode) -> {
-			itemByIdentText.put(identTextByIdentNodeMap.get(identNode), itemByItemNodeMap.get(itemNode));
-		});
-	}
-
-	/** Diese Methode TODO */
-	protected void checkIdentMap(Map<String, ?> itemByIdentTextMap, Getter<String, String> getErrorByIdentText, String... idents) {
-		for (var ident: idents) {
-			if (itemByIdentTextMap.get(ident) == null) throw new IllegalArgumentException(getErrorByIdentText.get(ident));
-		}
+		var identByNodeMap = new HashMap2<QN, String>(itemNodeByIdentNodeMap.size());
+		identLink.owner().newNodes(itemNodeByIdentNodeMap.keySet()).values(identByNodeMap::put);
+		var itemByNodeMap = HashMap2.from(Hashers.natural(), asItem);
+		itemNodeByIdentNodeMap.forEach((identNode, itemNode) -> itemByIdentMap.put(identByNodeMap.get(identNode), itemByNodeMap.install(itemNode)));
 	}
 
 	H2DM(H2DS parent, H2QN context) {
@@ -217,75 +196,12 @@ public class H2DM implements DM {
 		this.context = context;
 	}
 
-	private Translator2<QN, DL> linkTrans;
-
 	private HashMap2<String, H2DL> linkMap;
 
-	private Translator2<QN, DT> typeTrans;
+	private Translator2<QN, DL> linkTrans;
 
 	private HashMap2<String, H2DT> typeMap;
 
-	{}
-
-	protected class Setup {
-
-		public void putType(String ident, String label) {
-			this.typeSetupMap.put(ident, Arrays.asList(ident, label));
-		}
-
-		public void putLink(String ident, String label, String sourceTypeIdent, Handling sourceHandling, Multiplicity sourceMultiplicity, String targetTypeIdent,
-			Handling targetHandling, Multiplicity targetMultiplicity) {
-			this.linkSetupMap.put(ident, Arrays.asList(ident, label, //
-				sourceTypeIdent, Handling.trans.toSource(sourceHandling), Multiplicity.trans.toSource(sourceMultiplicity), //
-				targetTypeIdent, Handling.trans.toSource(targetHandling), Multiplicity.trans.toSource(targetMultiplicity)));
-		}
-
-		public void install() {
-			var that = H2DM.this;
-			var installValueNode = this.installValues();
-
-			// TODO
-
-			that.installLink(DT.IDENT_IsTypeWithIdent).setTargetMap(this.gm(this.typeSetupMap, 0, that::installTypeNode, installValueNode::get));
-			that.installLink(DT.IDENT_IsTypeWithLabel).setTargetMap(this.gm(this.typeSetupMap, 1, that::installTypeNode, installValueNode::get));
-
-			that.installLink(DL.IDENT_IsLinkWithIdent).setTargetMap(this.gm(this.linkSetupMap, 0, that::installLinkNode, installValueNode::get));
-			that.installLink(DL.IDENT_IsLinkWithLabel).setTargetMap(this.gm(this.linkSetupMap, 1, that::installLinkNode, installValueNode::get));
-			that.installLink(DL.IDENT_IsLinkWithSourceType).setTargetMap(this.gm(this.linkSetupMap, 2, that::installLinkNode, that::installTypeNode));
-			that.installLink(DL.IDENT_IsLinkWithSourceHandling).setTargetMap(this.gm(this.linkSetupMap, 3, that::installLinkNode, installValueNode::get));
-			that.installLink(DL.IDENT_IsLinkWithSourceMultiplicity).setTargetMap(this.gm(this.linkSetupMap, 4, that::installLinkNode, installValueNode::get));
-			that.installLink(DL.IDENT_IsLinkWithTargetType).setTargetMap(this.gm(this.linkSetupMap, 5, that::installLinkNode, that::installTypeNode));
-			that.installLink(DL.IDENT_IsLinkWithTargetHandling).setTargetMap(this.gm(this.linkSetupMap, 6, that::installLinkNode, installValueNode::get));
-			that.installLink(DL.IDENT_IsLinkWithTargetMultiplicity).setTargetMap(this.gm(this.linkSetupMap, 7, that::installLinkNode, installValueNode::get));
-
-			that.typesAsTypes().addAll(that.typeMap.values());
-			that.linksAsLinks().addAll(that.linkMap.values());
-		}
-
-		HashMap2<String, QN> installValues() {
-			var values = H2DM.this.owner().newValues(Iterables.concat(this.linkSetupMap.keySet().concat(this.typeSetupMap.keySet()),
-				Iterables.concatAll(this.linkSetupMap.values().concat(this.typeSetupMap.values()))).filter(Filters.empty()));
-			var result = new HashMap2<String, QN>();
-			values.putAll();
-			values.nodes(result::put);
-			return result;
-		}
-
-		HashMap2<QN, QN> gm(HashMap2<String, List<String>> setupMap, int index, Getter<String, QN> asKey, Getter<String, QN> asValue) {
-			var result = new HashMap2<QN, QN>();
-			setupMap.forEach((key, values) -> {
-				var value = values.get(index);
-				if (value != null) {
-					result.put(asKey.get(key), asValue.get(value));
-				}
-			});
-			return result;
-		}
-
-		final HashMap2<String, List<String>> linkSetupMap = new HashMap2<>();
-
-		final HashMap2<String, List<String>> typeSetupMap = new HashMap2<>();
-
-	}
+	private Translator2<QN, DT> typeTrans;
 
 }
