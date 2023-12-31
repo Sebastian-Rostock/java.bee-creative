32fddc204edb0d7033f13409863b446bb31ccabd
Sebastian Rostock <bee-creative@gmx.de>
2023-12-31 20:49:08 +0100

    translator2 extended

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/util/Translator2.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/util/Translator2.java
@@ -7,6 +7,26 @@ package bee.creative.util;
  * @param <GTarget> Typ der Zielobjekte. */
 public interface Translator2<GSource, GTarget> extends Translator<GSource, GTarget> {
 
+	/** Diese Methode ist eine Abkürzung für {@link Filters#from(Filter) Filters.from(this::isTarget)}. */
+	default Filter2<Object> isTarget() {
+		return Filters.from(this::isTarget);
+	}
+
+	/** Diese Methode ist eine Abkürzung für {@link Filters#from(Filter) Filters.from(this::isSource)}. */
+	default Filter2<Object> isSource() {
+		return Filters.from(this::isSource);
+	}
+
+	/** Diese Methode ist eine Abkürzung für {@link Getters#from(Getter) Getters.from(item -> this.toTarget(item))}. */
+	default Getter3<Object, GTarget> toTarget() {
+		return Getters.from(item -> this.toTarget(item));
+	}
+
+	/** Diese Methode ist eine Abkürzung für {@link Getters#from(Getter) Getters.from(item -> this.toSource(item))}. */
+	default Getter3<Object, GSource> toSource() {
+		return Getters.from(item -> this.toSource(item));
+	}
+
 	/** Diese Methode ist eine Abkürzung für {@link Translators#concat(Translator, Translator) Translators.concat(this, trans)}. */
 	default <GTarget2> Translator2<GSource, GTarget2> concat(Translator<GTarget, GTarget2> trans) throws NullPointerException {
 		return Translators.concat(this, trans);
