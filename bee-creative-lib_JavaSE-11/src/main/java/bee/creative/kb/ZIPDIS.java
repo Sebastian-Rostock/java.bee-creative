06df73ad2ea0ff843725799afd3d790fad6bb7cf
Sebastian Rostock <bee-creative@gmx.de>
2024-10-13 20:53:54 +0200

    javadoc ZIPDIS

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/kb/ZIPDIS.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/kb/ZIPDIS.java
@@ -3,17 +3,24 @@ package bee.creative.kb;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterInputStream;
 
+/** Diese Klasse implementiert einen {@link DataInputStream}, der einen gegebenen {@link InputStream} über einen {@link Inflater} entpackt.
+ * 
+ * @author [cc-by] 2024 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/] */
 public class ZIPDIS extends DataInputStream {
 
+	/** Dieser Konstruktor initialisiert den {@link InputStream} über {@link ByteArrayInputStream#ByteArrayInputStream(byte[]) new
+	 * ByteArrayInputStream(bytes)}. */
 	public ZIPDIS(byte[] bytes) throws IOException {
 		this(new ByteArrayInputStream(bytes));
 	}
 
-	public ZIPDIS(ByteArrayInputStream source) throws IOException {
+	/** Dieser Konstruktor initialisiert den gegebenen {@link InputStream}. */
+	public ZIPDIS(InputStream source) throws IOException {
 		super(new InflaterInputStream(source, new Inflater(true), 524288));
 	}
 
