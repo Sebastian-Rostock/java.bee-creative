1be5dcf1fca5e489611541f086b919ac54bf725c
Sebastian Rostock <bee-creative@gmx.de>
2025-01-03 10:53:47 +0100

    default iterator

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/lang/Array2.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/lang/Array2.java
@@ -1,13 +1,22 @@
 package bee.creative.lang;
 
+import bee.creative.util.Iterator2;
+import bee.creative.util.Iterators;
+
 /** Diese Schnittstelle definiert ein {@link #iterator() iterierbares} und nur lesbares Array mit {@link #size() Längenangabe}.
  *
- * @param <GItem> Typ der Elemente. */
+ * @param <GItem> Typ der Elemente.
+ * @author [cc-by] 2011 Sebastian Rostock [http://creativecommons.org/licenses/by/3.0/de/] */
 public interface Array2<GItem> extends Array<GItem>, Iterable<GItem> {
 
 	/** Diese Methode gibt die Anzahl der Elemente zurück.
 	 *
 	 * @return Elementanzahl. */
-	public int size();
+	int size();
+
+	@Override
+	default Iterator2<GItem> iterator() {
+		return Iterators.fromArray(this, 0, this.size());
+	}
 
 }
