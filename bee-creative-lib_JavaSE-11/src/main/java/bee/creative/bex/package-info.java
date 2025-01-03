219a8e0c35853b5de31f1e35c28b50891d62883f
Sebastian Rostock <bee-creative@gmx.de>
2025-01-03 10:52:14 +0100

    spelling

--- bee-creative-lib_JavaSE-11/src/main/java/bee/creative/bex/package-info.java
+++ bee-creative-lib_JavaSE-11/src/main/java/bee/creative/bex/package-info.java
@@ -1,2 +1,2 @@
-/** Dieser Namensraum definiert die Datenstrulturen und Algorithmen zum Erzeugen, Einlesen und Adaptieren der {@code BEX}-Datenformate. */
+/** Dieser Namensraum definiert die Datenstrukturen und Algorithmen zum Erzeugen, Einlesen und Adaptieren der {@code BEX}-Datenformate. */
 package bee.creative.bex;
